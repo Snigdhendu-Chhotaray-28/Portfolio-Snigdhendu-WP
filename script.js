77fd38303f02ef9d6aa6ed95a59111eafc2c1175
Snigdhendu-Chhotaray-28 <snigdhenduchhotaray@gmail.com>
2026-07-23 08:12:50 +0530

    Cache frequently used DOM elements

---
+++ script.js
@@ -0,0 +1,12 @@
+// =========================================================================
+// Global Constants and Utility Variables
+// =========================================================================
+
+// Cache frequently used DOM elements
+const bar = document.querySelector('.bar');
+const experienceBar = document.querySelector('.experience__bar');
+const barImg = document.querySelector('.experience__bar img');
+const eraser = document.querySelector('.erroser');
+const col1 = document.querySelector('.col1');
+const col2 = document.querySelector('.col2');
+
