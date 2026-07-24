41da8e7a2d4819de61ee85198d6c4645c458b2a2
Snigdhendu-Chhotaray-28 <snigdhenduchhotaray@gmail.com>
2026-07-24 08:28:39 +0530

    createStars function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 const bar = document.querySelector('.bar');
 const experienceBar = document.querySelector('.experience__bar');
 const barImg = document.querySelector('.experience__bar img');
-const eraser = document.querySelector('.erroser');
+const eraser = document.querySelector('.eraser');
 const col1 = document.querySelector('.col1');
 const col2 = document.querySelector('.col2');
 
@@ -24,4 +24,19 @@ let targetBarHeight = 0;
 // Variables for the Eraser animation
 let targetRotation = -50; 
 let currentRotation = -50;
+
+
+// =========================================================================
+// Star Blinking Animation Logic (NEWLY ADDED)
+// =========================================================================
+
+/**
+ * Creates and appends random stars to the star container.
+ * Stars are positioned using viewport units (vw/vh) and given randomized 
+ * animation properties to create a natural, twinkling effect.
+ * @param {number} count The number of stars to generate.
+ */
+function createStars(count) {
+        
+}
 
